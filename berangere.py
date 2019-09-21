10e310785d5b2704409f4bdef2c9d70e759bed26
furest <yannick.diepart@hotmail.com>
2019-09-21 15:31:04 +0200

    First commit

---
+++ berangere.py
@@ -0,0 +1,402 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+import sys
+import time
+import json
+import argparse
+from datetime import datetime
+import sqlite3
+
+def escapeshellarg(arg):
+    return "\\'".join("'" + p + "'" for p in arg.split("'"))
+
+class Berangere(commands.Bot):
+    
+    def __init__(self):
+        commands.Bot.__init__(self, command_prefix=config['command_prefix'], guild_subscriptions=True)
+        self.setup_commands()
+        self.saturation = dict()
+        self.volume = dict()
+        if not os.path.isfile("bot.db"):
+            conn = sqlite3.connect("bot.db")
+            cur = conn.cursor()
+            cur.execute("""create table users(
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                last_seen TEXT
+            )""")
+            conn.commit()
+            
+
+    async def is_authorized_channel(ctx):
+        """
+        Returns true if the name of the channel is in AUTHORIZED_CHANNEL_NAMES
+        """
+        channel = ctx.channel
+        if channel.type == discord.ChannelType.private:
+            return True
+        try:
+            return ctx.channel.name in config['authorized_channel_names']
+        except AttributeError as ex:
+            return False
+
+    async def is_guild_admin(ctx):
+        """
+        Returns true if the user that invoked the command is an admin
+        """
+        if type(ctx.author) is discord.Member:
+            return ctx.author.guild_permissions.administrator
+        else:
+            return False
+
+    def is_connected(ctx):
+        """
+        Returns true if the bot is connected in a channel of the same guild as the user that invoked the context
+        """
+        if ctx.guild.voice_client != None:
+            return True
+        return False
+
+    def is_playing(ctx):
+        """
+        Returns true if the bot is currently playing sound in the same guild as the user that invoked the context
+        """
+        if ctx.voice_client != None:
+            return ctx.voice_client.is_playing()
+        return False
+
+    def stop(ctx):
+        """
+        Stops the sound the bot is currently playing in the guild of the user that invoked the context
+        If the command implied that the bot had to disconnect after playing, the bot will disconnect as it finished playing the sound of the command.
+        """
+        if ctx.guild.voice_client != None:
+            ctx.guild.voice_client.stop()
+
+    def setup_commands(self):
+        """
+        Adds the commands to the bot
+        """
+
+        #Global checks (run before every command)
+        self.add_check(Berangere.is_authorized_channel)
+
+        @self.command()
+        async def ping(ctx):
+            """
+            Pong
+            """
+            await ctx.send("Pong")
+
+        @self.command()
+        async def sons(ctx):
+            """
+            Displays the list of sounds the bot knows
+            """
+            files = ['0'+os.path.splitext(f)[0] for f in os.listdir(config['sounds_base_dir']) if os.path.splitext(f)[1] == ".mp3"]
+            ret = config["separator"].join(files)
+            last_cut = 0
+            while len(ret[last_cut:]) > 1999:
+                next_cut = ret.rfind(config["separator"], last_cut, last_cut+1999)
+                msg = ret[last_cut:next_cut]
+                await ctx.send(msg)
+                last_cut = next_cut+len(config["separator"])
+            msg = ret[last_cut:]
+            await ctx.send(msg)
+                
+        
+        @self.command()
+        @commands.check(Berangere.is_guild_admin)
+        async def move(ctx, channel_name=None):
+            """
+            Moves the bot to the channel given in parameter
+            """
+            if Berangere.is_playing(ctx):
+                await ctx.send("Sorry but the bot is already busy playing")
+                return
+            if channel_name==None:
+                if ctx.author.voice.channel != None:
+                    dst_channel = ctx.author.voice.channel
+            else:
+                channel = [channel for channel in ctx.guild.voice_channels if channel.name==channel_name]
+                if len(channel) > 0:
+                    dst_channel = channel[0]
+                else:
+                    await ctx.send("Sorry but I do not know this channel")
+                    return
+            if ctx.voice_client != None:
+                await ctx.voice_client.move_to(dst_channel)
+            else:
+                await dst_channel.connect()
+            
+        @self.command()
+        async def stop(ctx):
+            """
+            Check if the bot is playing in the same channel as the user and disconnects it from the channel
+            """
+            if Berangere.is_playing(ctx):
+                Berangere.stop(ctx)
+            else:
+                await ctx.send("There is nothing to stop")
+        
+        @self.command()
+        async def kick(ctx):
+            """
+            Kick the bot if connected in the same guild as the user
+            """
+            if ctx.voice_client != None:
+                await ctx.voice_client.disconnect()
+
+        @self.command()
+        async def playAll(ctx, directory=config['sounds_base_dir']):
+            """
+            Plays all sounds in the default directory or in the directory specified
+            """
+            try:
+                must_disconnect=False
+                all_songs = [f for f in os.listdir(f"./{directory}") if os.path.splitext(f)[-1] == ".mp3"]
+                if ctx.voice_client == None:
+                    await ctx.author.voice.channel.connect()
+                    must_disconnect=True
+                elif ctx.voice_client.channel != ctx.author.voice.channel:
+                    #If not in same channel, check permissions
+                    if type(ctx.author) is not discord.Member or not ctx.author.guild_permissions.administrator:
+                        #User has no admin rights. Refuse to play sound
+                        await ctx.send("Sorry but the bot is in another channel")
+                        return
+                #await self.change_presence(activity=discord.Activity(name="Gros bordel", type=discord.ActivityType.listening), status=discord.Status.do_not_disturb)
+                await self.playSounds(ctx=ctx,directory=directory,sounds=all_songs, disconnect_after=must_disconnect)
+            except commands.BadArgument as ex:
+                print(ex)
+                await ctx.send(ex)
+            except AttributeError as ex:
+                print(ex)
+                await ctx.send("Try sending from a channel on your guild")
+            except discord.errors.ClientException as ex:
+                print(ex)
+                #Not member of the guild or busy playing in same guild
+                await ctx.send("Sorry but the bot is in another channel")
+                return
+
+        @self.command()
+        async def ls(ctx, *sounds):
+            """
+            Plays the list of sounds sounds simultaneously
+            """
+            try:
+                must_disconnect=False
+                user_voice_channel = ctx.author.voice.channel
+                sounds = [sound+".mp3" for sound in sounds if os.path.isfile(f"{config['sounds_base_dir']}/{sound}.mp3")]
+                if ctx.voice_client == None:
+                    await ctx.author.voice.channel.connect()
+                    must_disconnect=True
+                elif ctx.voice_client.channel != ctx.author.voice.channel:
+                    #If not in same channel, check permissions
+                    if type(ctx.author) is not discord.Member or not ctx.author.guild_permissions.administrator:
+                        #User has no admin rights. Refuse to play sound
+                        await ctx.send("Sorry but the bot is in another channel")
+                        return
+                await self.playSounds(ctx=ctx,directory=config['sounds_base_dir'],sounds=sounds, disconnect_after=must_disconnect)
+            except commands.BadArgument as ex:
+                print(ex)
+                await ctx.send(ex)
+            except AttributeError as ex:
+                print(ex)
+                await ctx.send("Try sending from a channel on your guild")
+            except discord.errors.ClientException as ex:
+                print(ex)
+                #Not member of the guild or busy playing in same guild
+                await ctx.send("Sorry but the bot is in another channel")
+                return
+
+        @self.command()
+        async def mu(ctx, sound, number, interval):
+            """
+            Plays the given sound {number} times at an interval of {interval} milliseconds
+            """
+            try:
+                must_disconnect=False
+                if ctx.voice_client == None:
+                    await ctx.author.voice.channel.connect()
+                    must_disconnect=True
+                elif ctx.voice_client.channel != ctx.author.voice.channel:
+                    #If not in same channel, check permissions
+                    if type(ctx.author) is not discord.Member or not ctx.author.guild_permissions.administrator:
+                        #User has no admin rights. Refuse to play sound
+                        await ctx.send("Sorry but the bot is in another channel")
+                        return
+                if not os.path.isfile(f"{config['sounds_base_dir']}/{sound}.mp3"):
+                    await ctx.send("This sound does not exist")
+                    return
+                if str(number).isdigit() and str(interval).isdigit():
+                    repeat=int(number)
+                    delay = float(interval)/1000
+                    plays_done = 0
+                    loop = asyncio.get_running_loop()
+                    before_options = f'-filter_complex "volume={self.saturation.get(ctx.guild, 1)}"'
+                    volume=self.volume.get(ctx.guild, 1)
+                    def repeat_or_disconnect(err):
+                        nonlocal plays_done
+                        plays_done = plays_done + 1
+                        if plays_done == repeat:
+                            loop.create_task(ctx.voice_client.disconnect(force=True))
+                        else:
+                            time.sleep(delay)
+                            audio_source = discord.FFmpegPCMAudio(options=before_options, source=f"{config['sounds_base_dir']}/{sound}.mp3")
+                            audio_volume = discord.PCMVolumeTransformer(audio_source, volume=volume)
+                            ctx.voice_client.play(audio_volume, after=repeat_or_disconnect)
+                    audio_source = discord.FFmpegPCMAudio(options=before_options, source=f"{config['sounds_base_dir']}/{sound}.mp3")
+                    audio_volume = discord.PCMVolumeTransformer(audio_source, volume=volume)
+                    ctx.voice_client.play(audio_volume, after=repeat_or_disconnect)
+            except AttributeError as ex:
+                print(ex)
+                await ctx.send("Try sending from a channel on your guild")
+            except discord.errors.ClientException as ex:
+                print(ex)
+                #Not member of the guild or busy playing in same guild
+                await ctx.send("Sorry but the bot is in another channel")
+                return
+
+        @self.command()
+        async def saturation(ctx, value):
+            try:
+                self.saturation[ctx.guild] = float(value)
+                await ctx.send(f"Saturation set to {value}")
+            except:
+                await ctx.send("Value must be a number")
+        @self.command()
+        async def volume(ctx, value):
+            try:
+                self.volume[ctx.guild] = float(value)
+                await ctx.send(f"Volume set to {value}")
+            except:
+                await ctx.send("Value must be a number")
+                
+        @self.command()
+        async def lastSeen(ctx, username):
+            conn = sqlite3.connect("bot.db")
+            curs = conn.cursor()
+            curs.execute("SELECT last_seen FROM users WHERE name = ?", (username,))
+            user = curs.fetchone()
+            if user is None:
+                await ctx.send(f"{username}? Never heard of such a name...")
+            else:
+                await ctx.send(f"{username} was last seen on {user[0]}")
+
+    async def playSounds(self, ctx,directory, sounds, disconnect_after=True):
+        if len(sounds) == 0:
+            raise commands.BadArgument("Sorry but there is no sound to play:(")
+        elif len(sounds) > 1:
+            nb_sounds = len(sounds)
+            source_str = ""
+            for sound in sounds[1:]:
+                song = escapeshellarg(directory + "/" + sound)
+                source_str += f"-i {song} "
+            before_options = f'-filter_complex "amix=inputs={nb_sounds}:duration=longest[a];[a]volume={self.saturation.get(ctx.guild, 1)}" {source_str}'
+        else:
+            before_options = f'-filter_complex "volume={self.saturation.get(ctx.guild, 1)}"'
+       
+        volume=self.volume.get(ctx.guild, 1)
+        audio_source = discord.FFmpegPCMAudio(before_options=before_options, source=f"{directory}/{sounds[0]}")
+        audio_volume = discord.PCMVolumeTransformer(audio_source, volume=volume)
+        loop = asyncio.get_running_loop()
+        def disconnect(error):
+            #loop.create_task(ctx.change_presence(discord.Activity(""), status=discord.Status.online))
+            if disconnect_after == True:
+                loop.create_task(ctx.voice_client.disconnect(force=True))
+        ctx.voice_client.play(audio_volume, after=disconnect)
+    async def on_command_error(self, ctx, exc):
+        """
+        Called when an uncaught exception is thrown in a command
+        """
+        if type(exc) is commands.CommandNotFound:
+            #When a command does not exist, try to play the sound named like the command.
+            command = ctx.invoked_with
+            if not os.path.isfile(f"{config['sounds_base_dir']}/{command}.mp3"):
+                await ctx.send(f"{command} is not a valid sound.")
+                return
+            try:
+                must_disconnect=False
+                if ctx.voice_client == None:
+                    await ctx.author.voice.channel.connect()
+                    must_disconnect=True
+                elif ctx.voice_client.channel != ctx.author.voice.channel:
+                    #If not in same channel, check permissions
+                    if type(ctx.author) is not discord.Member or not ctx.author.guild_permissions.administrator:
+                        #User has no admin rights. Refuse to play sound
+                        await ctx.send("Sorry but the bot is in another channel")
+                        return
+                user_voice_channel = ctx.author.voice.channel
+                await self.playSounds(ctx=ctx,directory=config['sounds_base_dir'], sounds=[command+".mp3"], disconnect_after=must_disconnect)
+            except AttributeError as ex:
+                print(ex)
+                await ctx.send("Try sending from a channel on your guild")
+            except discord.errors.ClientException as ex:
+                #Not member of the guild or busy playing in same guild
+                print(ex)
+                await ctx.send("Sorry but the bot is in another channel")
+                return
+        elif type(exc) is commands.MissingRequiredArgument:
+            await ctx.send(ctx.command.usage)
+        else:
+            print(exc)
+    async def on_voice_state_update(self, member, before, after):
+        chan = [c for c in member.guild.text_channels if c.name =="voice-log"]
+        if len(chan) == 0:
+            return
+        chan = chan[0]
+        if before.channel == after.channel:
+            return
+        now = datetime.now().strftime("%H:%M:%S")
+        if before.channel == None:
+            #Connected
+            await chan.send(f"**{member.display_name}** connected to channel **{after.channel.name}** at {now}")
+        elif after.channel == None:
+            #Disconnected
+            await chan.send(f"**{member.display_name}** disconnected from channel **{before.channel.name}** at {now}")
+            conn = sqlite3.connect("bot.db")
+            curs = conn.cursor()
+            curs.execute("UPDATE users set last_seen = ? WHERE name=?", (datetime.now().strftime('%d/%m/%Y %H:%M:%S'),member.name))
+            if curs.rowcount == 0:
+                curs.execute("INSERT INTO users (name, last_seen) values(?,?)", (member.name, datetime.now().strftime('%d/%m/%Y %H:%M:%S')))
+            conn.commit()
+        else:
+            #changed channel
+            await chan.send(f"**{member.display_name}** moved from channel **{before.channel.name}** to **{after.channel.name}** at {now}")
+
+    async def on_ready(self):
+        print(f"Bot connected with name {self.user.name}")
+
+    async def on_message(self, message):
+        if message.content.startswith(config['command_prefix']):
+            await super().on_message(message)
+            return
+        if message.channel.name not in config['authorized_channel_names'] or message.author == self.user:
+            return
+        await message.channel.send(f"I'm a bot you dumbass. If you're here for help type \"{config['command_prefix']}help\" like everyone else")
+        await message.add_reaction("😡")
+
+
+if __name__ == "__main__":
+    conf_file = open("config.json", "r")
+    config = json.load(conf_file)
+    parser = argparse.ArgumentParser(description="Discord bot that plays sound files.")
+    parser.add_argument("-k","--key", metavar="key", type=str, nargs=1, help="Name of the key as written in the config file.")
+    args = parser.parse_args()
+    keyname = args.key
+    if keyname != None:
+        try:
+            KEY=config['keys'][keyname]
+        except:
+            print(f"There is no key named {keyname}. Please check config.json")
+            sys.exit()
+    else:
+        try:
+            KEY = list(config['keys'].values())[0]
+        except:
+            print("No key found in config.json")
+            sys.exit()
+    ber = Berangere()
+    ber.run(KEY)
